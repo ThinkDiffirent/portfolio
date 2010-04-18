9cc790ad543845a7ac9dd192a233c17802ed8a55
Nicolas Blanco <slainer68@gmail.com>
2010-04-18 21:50:27 +0200

    localized tags.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,4 @@
 class PagesController < ApplicationController
   def index
-    @tags = %w(web ruby methodologie passion agile open\ source libre rails chaine\ de\ production fun user\ experience TDD unix danse
-    SaaS choucroute intégration\ continue administration MacOs\ X standards programmation\ objet standards photo git).shuffle
   end
 end
