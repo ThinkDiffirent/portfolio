a6819b047b3ba5e7a899a6503463c88aa7454853
Nicolas Blanco <slainer68@gmail.com>
2011-11-09 10:55:56 +0100

    Rescue 404

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
+  rescue_from ActionView::MissingTemplate do |e|
+    render :file => 'public/404.html', :status => 404, :layout => false if Rails.env.production?
+  end
+
   def method_missing(*args)
     render :action => params[:action]
   end
 end
-
